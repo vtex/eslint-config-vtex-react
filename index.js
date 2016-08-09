41ddef903670de96fdfb3d97371e6113a4c758e5
Breno Calazans <breno@vtex.com.br>
2016-08-09 11:56:15 -0300

    Use more reasonable rules

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ module.exports = {
   "rules": {
     "react/display-name": 2,
     // "react/forbid-prop-types": not-defined
-    "react/no-danger": 2,
+    // "react/no-danger": 0, the prop is already clear that its use it's dangerous
     "react/no-deprecated": 2,
-    "react/no-did-mount-set-state": [2, "allow-in-func"],
-    "react/no-did-update-set-state": [2, "allow-in-func"],
+    // "react/no-did-mount-set-state": 0,
+    // "react/no-did-update-set-state": 0,
     "react/no-direct-mutation-state": 2,
     "react/no-is-mounted": 2,
     // "react/no-multi-comp": not-defined
@@ -20,7 +20,9 @@ module.exports = {
     "react/prop-types": 2,
     "react/react-in-jsx-scope": 2,
     "react/require-render-return": 2,
-    "react/self-closing-comp": 2,
+    "react/self-closing-comp": [2, {
+      "html": false
+    }],
     // "react/sort-comp": not-defined
     "react/sort-prop-types": [2, {
       "callbacksLast": true,
@@ -42,16 +44,16 @@ module.exports = {
     "react/jsx-indent-props": [2, 2],
     "react/jsx-key": 2,
     // "react/jsx-max-props-per-line": not-defined
-    "react/jsx-no-bind": 2,
+    "react/jsx-no-bind": [2, {
+      "ignoreRefs": true,
+      "allowArrowFunctions": true
+    }],
     "react/jsx-no-duplicate-props": 2,
     // "react/jsx-no-literals": not-defined
     // "react/jsx-no-target-blank": not-defined
     "react/jsx-no-undef": 2,
     "react/jsx-pascal-case": [2],
-    "react/jsx-sort-props": [2, {
-      "callbacksLast": true,
-      "ignoreCase": false
-    }],
+    // "react/jsx-sort-props": 0,
     "react/jsx-space-before-closing": 2,
     "react/jsx-uses-react": 2,
     "react/jsx-uses-vars": 2
