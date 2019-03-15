e0533e6dc7f646c3c051c8a64a447b51510805a0
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-15 10:28:51 -0300

    Add extends to base config

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
 module.exports = {
-  "rules": {
-    "react/display-name": 2,
-    // "react/forbid-prop-types": not-defined
-    // "react/no-danger": 0, the prop is already clear that its use it's dangerous
-    "react/no-deprecated": 2,
-    // "react/no-did-mount-set-state": 0,
-    // "react/no-did-update-set-state": 0,
-    "react/no-direct-mutation-state": 2,
-    "react/no-is-mounted": 2,
-    // "react/no-multi-comp": not-defined
-    // "react/no-set-state": not-defined
-    "react/no-string-refs": 2,
-    "react/no-unknown-property": 2,
-    "react/prefer-es6-class": [2, "always"],
-    "react/prefer-stateless-function": 0,
-    "react/prop-types": 2,
-    "react/react-in-jsx-scope": 2,
-    "react/require-render-return": 2,
-    "react/self-closing-comp": [2, {
-      "html": false
-    }],
-    // "react/sort-comp": not-defined
-    // "react/sort-prop-types": 0, this is a pain in the arse
-    // JSX Rules
-    "react/jsx-wrap-multilines": 1,
-    "react/jsx-boolean-value": 2,
-    // "react/jsx-closing-bracket-location": not-defined
-    "react/jsx-curly-spacing": [2, "never"],
-    "react/jsx-equals-spacing": [2, "never"],
-    // "react/first-prop-new-line": not-defined
-    "react/jsx-handler-names": [2, {
-      "eventHandlerPrefix": "handle",
-      "eventHandlerPropPrefix": "on"
-    }],
-    "react/jsx-indent": [2, 2],
-    "react/jsx-indent-props": [2, 2],
-    "react/jsx-key": 2,
-    // "react/jsx-max-props-per-line": not-defined
-    "react/jsx-no-bind": [2, {
-      "ignoreRefs": true,
-      "allowArrowFunctions": true
-    }],
-    "react/jsx-no-duplicate-props": 2,
-    // "react/jsx-no-literals": not-defined
-    // "react/jsx-no-target-blank": not-defined
-    "react/jsx-no-undef": 2,
-    "react/jsx-pascal-case": [2],
-    // "react/jsx-sort-props": 0,
-    "react/jsx-tag-spacing": 2,
-    "react/jsx-uses-react": 2,
-    "react/jsx-uses-vars": 2,
-    "react-hooks/rules-of-hooks": "error"
+  extends: [
+    'vtex',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/react',
+  ],
+  plugins: ['react', 'jsx-a11y', 'react-hooks'],
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  "plugins": [
-    "react",
-    "react-hooks"
-  ]
-};
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    linkComponents: [{ name: 'Link', linkAttribute: 'to' }],
+  },
+
+  rules: {
+    // 'react/forbid-prop-types': not-defined
+    // 'react/no-danger': 'off', the prop is already clear that its use it's dangerous
+    // 'react/no-did-mount-set-state': 'off',
+    // 'react/no-did-update-set-state': 'off',
+    // 'react/no-multi-comp': not-defined
+    // 'react/no-set-state': not-defined
+    'react/prefer-es6-class': ['error', 'always'],
+    'react/prefer-stateless-function': 'off',
+    'react/prop-types': ['error', { skipUndeclared: true }],
+    'react/self-closing-comp': [
+      'error',
+      {
+        html: false,
+      },
+    ],
+    // 'react/sort-comp': not-defined
+    // 'react/sort-prop-types': 'off', this is a pain in the arse
+
+    // JSX Rules
+    'react/jsx-wrap-multilines': 'warn',
+    'react/jsx-boolean-value': 'error',
+    // 'react/jsx-closing-bracket-location': not-defined
+    'react/jsx-curly-spacing': ['error', 'never'],
+    'react/jsx-equals-spacing': ['error', 'never'],
+    // 'react/first-prop-new-line': not-defined
+    'react/jsx-handler-names': [
+      'error',
+      {
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
+      },
+    ],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    // 'react/jsx-max-props-per-line': not-defined
+    'react/jsx-no-bind': [
+      'error',
+      {
+        ignoreRefs: true,
+        allowArrowFunctions: true,
+      },
+    ],
+    // 'react/jsx-no-literals': not-defined
+    'react/jsx-pascal-case': ['error'],
+    // 'react/jsx-sort-props': 'off',
+    'react/jsx-tag-spacing': 'error',
+
+    // Hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
